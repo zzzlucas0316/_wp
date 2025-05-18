92c20952687924ba5fa32ce59716085260363b67
zzzlucas0316 <zzz228886950316@gmail.com>
2025-05-18 16:19:47 +0800

    完成習題7

---
+++ homework/習題7/7-1.js
@@ -0,0 +1,51 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import * as render from './render.js'
+
+const posts = [
+
+];
+
+const router = new Router();
+
+router.get('/', list)
+    .get('/post/new', add)
+    .get('/post/:id', show)
+    .post('/post', create);
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+async function list(ctx) {
+    ctx.response.body = await render.list(posts);
+}
+
+async function add(ctx) {
+    ctx.response.body = await render.newPost();
+}
+
+async function show(ctx) {
+    const id = ctx.params.id;
+    const post = posts[id];
+    if (!post) ctx.throw(404, 'invalid post id');
+    ctx.response.body = await render.show(post);
+}
+
+async function create(ctx) {
+    const body = ctx.request.body
+    if (body.type() === "form") {
+        const pairs = await body.form()
+        const post = {}
+        for (const [key, value] of pairs) {
+            post[key] = value
+        }
+        console.log('post=', post)
+        const id = posts.push(post) - 1;
+        post.created_at = new Date(); // 設定建立時間
+        post.id = id;
+        ctx.response.redirect('/');
+    }
+}
+
+console.log('Server run at http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
